b53b9104e309ff0835086ec065adb93921dbaaa0
Thomas Burton <tburton5572@gmail.com>
2020-10-12 15:32:26 +0100

    Added Van der Pol file for testing purposes

---
+++ van_der_pol.py
@@ -0,0 +1,4 @@
+# This file contains the functions defining the dynamical system of the Van der Pol equations,
+# the proposed solution curve, and the real solution (limit cycle).
+
+# Thomas Burton - October 2020
