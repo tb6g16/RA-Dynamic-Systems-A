71b8256f105aa739b82bd3a8e8a145538a5003b6
Thomas Burton <tburton5572@gmail.com>
2020-10-14 23:57:21 +0100

    Added 2D state-space plotting function

--- plotting.py
+++ plotting.py
@@ -6,7 +6,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot(dynamics, curve, dim = 2):
+def plot(dynamics, curve):
     """
         This function plots the given dynamical system and trajectory in the same space, as a vector
         field and as a curve respectively.
@@ -18,22 +18,46 @@ def plot(dynamics, curve, dim = 2):
             each state
         curve: function
             a given trajectory in state-space
-        dim: {2, 3}
-            the dimension of the state-space being plotted in, only
-            permitted to be 2- or 3-dimensional
     """
 
-    if dim not in (2, 3):
-        raise ValueError("Dimension given not a valid option, choose from 2 or 3!")
+    # define some constants
+    s_d = 200
+    x_d = 30
+    y_d = 30
 
-    # discretise domain on trajectory
-    s = np.linspace(0, 2*np.pi, 200)
+    # initialise vectors
+    trajectory_x = np.zeros([s_d])
+    trajectory_x_dot = np.zeros([s_d])
+    response_x = np.zeros([x_d, y_d])
+    response_x_dot = np.zeros([x_d, y_d])
 
-    if dim == 2:
-        # discretise domain of dynamical system
-        X, Y = np.meshgrid(np.linspace(-5, 5, 200), np.linspace(-5, 5, 200))
-        
-    else:
-        s[0] = 1
+    # discretise domain on trajectory and of the dynamical system
+    s = np.linspace(0, 2*np.pi, s_d)
+    X, X_dot = np.meshgrid(np.linspace(-1.5, 1.5, x_d), np.linspace(-1.5, 1.5, y_d))
+
+    # generate trajectory
+    for i in range(s_d):
+        trajectory_x[i] = curve(s[i])[0]
+        trajectory_x_dot[i] = curve(s[i])[1]
+
+    # calculate response of the dynamical system at each state
+    for i in range(x_d):
+        for j in range(y_d):
+            response_x[i][j] = dynamics([X[i][j], X_dot[i][j]])[0]
+            response_x_dot[i][j] = dynamics([X[i][j], X_dot[i][j]])[1]
+    
+    # plot dynamical system
+    plt.figure()
+    ax = plt.gca()
+    ax.quiver(X, X_dot, response_x, response_x_dot)
+    ax.plot(trajectory_x, trajectory_x_dot)
+    plt.xlabel(r"$x$"), plt.ylabel(r"$\dot{x}$")
+    ax.set_aspect("equal")
+    plt.show()
 
     return None
+
+if __name__ == "__main__":
+    from test_case import dynamical_system, solution_curve
+
+    plot(dynamical_system, solution_curve)
