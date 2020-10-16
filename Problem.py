93bd985d5d1bfb90773f55e3a928c28313c70f67
Thomas Burton <tburton5572@gmail.com>
2020-10-16 11:16:29 +0100

    Added skeleton 'dynamicalSystem' and 'Problem' classes

---
+++ Problem.py
@@ -0,0 +1,10 @@
+# This file contains the object that holds all the important information about
+# the optimisation problem. It will have the functionality to optimise the
+# global residual in all the domains, and analyse the data too.
+
+# Thomas Burton - October 2020
+
+class Problem:
+
+    def __init__(self):
+        self.dummy = None
---
+++ dynamicalSystem.py
@@ -0,0 +1,9 @@
+# This file contains the definition of the class that defines the dynamical
+# system in state-space.
+
+# Thomas Burton - October 2020
+
+class dynamicalSystem:
+
+    def __init__(self):
+        self.dummy = None
