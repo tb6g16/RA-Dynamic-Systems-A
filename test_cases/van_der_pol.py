699c897bcb3738df865fdf4e6e334078cb294286
Thomas Burton <tburton5572@gmail.com>
2020-10-18 20:10:09 +0100

    Implemented Van-der-Pol oscillator

--- test_cases/van_der_pol.py
+++ test_cases/van_der_pol.py
@@ -2,3 +2,15 @@
 # the proposed solution curve, and the real solution (limit cycle).
 
 # Thomas Burton - October 2020
+
+import numpy as np
+
+def g(x):
+    # nonlinear damping coefficient
+    mu = 0
+    # initialise response vector
+    response = np.zeros(np.shape(x))
+    # assign response
+    response[0] = x[1]
+    response[1] = (mu*(1 - (x[0] ** 2))*x[1]) + x[0]
+    return response
