b950e26eb224bf05f355a0951b01beea2aa99b34
Thomas Burton <tburton5572@gmail.com>
2020-10-18 22:14:20 +0100

    Tidy up of van_der_pol.py

--- test_cases/van_der_pol.py
+++ test_cases/van_der_pol.py
@@ -1,16 +1,23 @@
-# This file contains the functions defining the dynamical system of the Van der Pol equations,
-# the proposed solution curve, and the real solution (limit cycle).
+# This file contains the functions defining the dynamical system of the Van der
+# Pol equations, the proposed solution curve, and the real solution (limit
+# cycle).
 
 # Thomas Burton - October 2020
 
 import numpy as np
 
-def g(x):
-    # nonlinear damping coefficient
-    mu = 1
+# define optional arguments
+defaults = {'mu': 0, 'a': np.pi}
+
+def g(x, defaults = defaults):
+    # unpack defaults
+    mu = defaults['mu']
     # initialise response vector
     response = np.zeros(np.shape(x))
     # assign response
     response[0] = x[1]
     response[1] = (mu*(1 - (x[0] ** 2))*x[1]) - x[0]
     return response
+
+if __name__ == "__main__":
+    print(g([0, 1]))
