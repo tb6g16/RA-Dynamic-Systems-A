d421ddf285cca231a28828da95a53f355a810d24
Thomas Burton <tburton5572@gmail.com>
2020-10-22 14:34:52 +0100

    Created plotting classes to make plotting results easier down the line

---
+++ systemPlotting.py
@@ -0,0 +1,10 @@
+# This file contains the definition for a class giving all the plotting
+# information required to easily plot an instance of the System class.
+
+# Thomas Burton - October 2020
+
+# MAYBE MAKE IT A SUBCLASS OF A MORE GENERAL PLOTTING CLASS???
+
+class systemPlotting:
+    def __init__(self):
+        pass
