fad84ba2b876dc6089312fd4e1279c4acdb1d30c
Thomas Burton <tburton5572@gmail.com>
2020-11-25 13:58:41 +0000

    Completed tests for traj_prod(), gradient(), and traj_response()

--- test_cases/viswanath.py
+++ test_cases/viswanath.py
@@ -15,8 +15,8 @@ def response(x, defaults = defaults):
     response = np.zeros(x.shape)
 
     # assign response
-    response[0] = -x[1] + (mu*x[0])*(1 - np.sqrt((x[0]**2) + (x[1]**2)))
-    response[1] = x[0] + (mu*x[1])*(1 - np.sqrt((x[0]**2) + (x[1]**2)))
+    response[0] = x[1] + (mu*x[0])*(1 - np.sqrt((x[0]**2) + (x[1]**2)))
+    response[1] = -x[0] + (mu*x[1])*(1 - np.sqrt((x[0]**2) + (x[1]**2)))
 
     return response
 
@@ -31,8 +31,8 @@ def jacobian(x, defaults = defaults):
     # compute jacobian elements
     r = np.sqrt((x[0]**2) + (x[1]**2))
     jacobian[0, 0] = mu*(1 - (2*(x[0]**2) + (x[1]**2))/r)
-    jacobian[0, 1] = -1 - (mu*x[0]*x[1])/r
-    jacobian[1, 0] = 1 - (mu*x[0]*x[1])/r
+    jacobian[0, 1] = 1 - (mu*x[0]*x[1])/r
+    jacobian[1, 0] = -1 - (mu*x[0]*x[1])/r
     jacobian[1, 1] = mu*(1 - ((x[0]**2) + 2*(x[1]**2))/r)
 
     return jacobian
--- unit_tests/TestTrajectory.py
+++ unit_tests/TestTrajectory.py
@@ -10,6 +10,7 @@ from System import System
 from test_cases import unit_circle as uc
 from test_cases import ellipse as elps
 from test_cases import van_der_pol as vpd
+from test_cases import viswanath as vis
 
 class TestTrajectory(unittest.TestCase):
     
@@ -19,15 +20,16 @@ class TestTrajectory(unittest.TestCase):
         new_traj1 = traj1.traj_prod(traj2)
         new_traj2 = traj2.traj_prod(traj1)
 
-        # new trajectories are instances of Trajectory class
-        self.assertTrue(isinstance(new_traj1, Trajectory))
-        self.assertTrue(isinstance(new_traj2, Trajectory))
+        # output is of the Trajectory class
+        self.assertIsInstance(new_traj1, Trajectory)
+        self.assertIsInstance(new_traj2, Trajectory)
 
         # does the operation commute
         self.assertEqual(new_traj1, new_traj2)
 
         # inner product equal to norm
-        self.assertEqual(traj1.normed_traj(), traj1.traj_prod(traj1))
+        self.assertEqual(traj1.normed_traj()**2, traj1.traj_prod(traj1))
+        self.assertEqual(traj2.normed_traj()**2, traj2.traj_prod(traj2))
 
         # single number at each index
         temp = True
@@ -37,10 +39,63 @@ class TestTrajectory(unittest.TestCase):
         self.assertTrue(temp)
 
     def test_gradient(self):
-        pass
+        traj1 = Trajectory(uc.x)
+        traj2 = Trajectory(elps.x)
+
+        # same shape as original trajectories
+        self.assertEqual(traj1.curve_array.shape, traj1.grad.curve_array.shape)
+        self.assertEqual(traj2.curve_array.shape, traj2.grad.curve_array.shape)
+
+        # correct values
+        traj1_grad = np.zeros(traj1.curve_array.shape)
+        traj2_grad = np.zeros(traj2.curve_array.shape)
+        for i in range(traj2.curve_array.shape[1]):
+            s = ((2*np.pi)/traj2.curve_array.shape[1])*i
+            traj1_grad[0, i] = -np.sin(s)
+            traj1_grad[1, i] = -np.cos(s)
+            traj2_grad[0, i] = -2*np.sin(s)
+            traj2_grad[1, i] = -np.cos(s)
+        traj1_grad = Trajectory(traj1_grad)
+        traj2_grad = Trajectory(traj2_grad)
+        self.assertEqual(traj1_grad, traj1.grad)
+        self.assertEqual(traj2_grad, traj2.grad)
 
     def test_traj_response(self):
-        pass
+        traj1 = Trajectory(uc.x)
+        traj2 = Trajectory(elps.x)
+        sys1 = System(vpd)
+        sys2 = System(vis)
+        traj1_response1 = traj1.traj_response(sys1)
+        traj1_response2 = traj1.traj_response(sys2)
+        traj2_response1 = traj2.traj_response(sys1)
+        traj2_response2 = traj2.traj_response(sys2)
+        
+        # output is of the Trajectory class
+        self.assertIsInstance(traj1_response1, Trajectory)
+        self.assertIsInstance(traj1_response2, Trajectory)
+        self.assertIsInstance(traj2_response1, Trajectory)
+        self.assertIsInstance(traj2_response2, Trajectory)
+
+        # same response for trajectories at crossing points
+        cross_index1 = int(((traj1.curve_array.shape[1])/(2*np.pi))*(np.pi/2))
+        cross_index2 = int(((traj1.curve_array.shape[1])/(2*np.pi))*((3*np.pi)/2))
+        traj1_cross1_response1 = traj1_response1.curve_array[:, cross_index1]
+        traj2_cross1_response1 = traj2_response1.curve_array[:, cross_index1]
+        traj1_cross2_response1 = traj1_response1.curve_array[:, cross_index2]
+        traj2_cross2_response1 = traj2_response1.curve_array[:, cross_index2]
+        traj1_cross1_response2 = traj1_response2.curve_array[:, cross_index1]
+        traj2_cross1_response2 = traj2_response2.curve_array[:, cross_index1]
+        traj1_cross2_response2 = traj1_response2.curve_array[:, cross_index2]
+        traj2_cross2_response2 = traj2_response2.curve_array[:, cross_index2]
+        self.assertTrue(np.allclose(traj1_cross1_response1, traj2_cross1_response1))
+        self.assertTrue(np.allclose(traj1_cross2_response1, traj2_cross2_response1))
+        self.assertTrue(np.allclose(traj1_cross1_response2, traj2_cross1_response2))
+        self.assertTrue(np.allclose(traj1_cross2_response2, traj2_cross2_response2))
+
+        # gradient of solution equal to response
+        sys2.parameters['mu'] = 1
+        self.assertEqual(traj1.grad, traj1_response1)
+        self.assertEqual(traj1.grad, traj1_response2)
 
     def test_traj_nl_response(self):
         pass
